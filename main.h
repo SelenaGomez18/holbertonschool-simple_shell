0c89ff09275f73047c007f792f9439c506b377c4
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-08-01 19:36:36 -0500

    Add changes to shell.c and add a new file for clean spaces

--- main.h
+++ main.h
@@ -5,11 +5,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <string.h>
+
 extern char **environ;
 
 char *read_line(void);
 void execute_cmd(char *cmd);
 void display_prompt(void);
+char *trim_spaces(char *str);
 
 
 #endif /*MAIN_H*/
--- shell.c
+++ shell.c
@@ -1,26 +1,32 @@
 #include "main.h"
 
+/**
+ * main - Loop principal del shell
+ * Return: 0 al salir
+ */
 int main(void)
 {
-    char *line = NULL;
+	char *line = NULL, *clean_line;
 
-    while (1)
-    {
-        display_prompt();
-        line = read_line();
+	while (1)
+	{
+		display_prompt();
+		line = read_line();
 
-        if (line == NULL)
-        {
-            if (isatty(STDIN_FILENO))
-                write(STDOUT_FILENO, "\n", 1);
-            break;
-        }
+		if (line == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
 
-        if (line[0] != '\0')
-            execute_cmd(line);
+		clean_line = trim_spaces(line);
 
-        free(line);
-    }
-    
-    return(0);
+		if (clean_line[0] != '\0')
+			execute_cmd(clean_line);
+
+		free(line);
+	}
+
+	return (0);
 }
