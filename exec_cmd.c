838c7ee2ee25671eae26678644192456cad39248
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-08-05 19:44:53 -0500

    Add changes to shell.c and add a new file for clean spaces

--- exec_cmd.c
+++ exec_cmd.c
@@ -4,66 +4,39 @@ int execute_command(char *command, char **args, char **env)
 {
 	pid_t pid;
 	int status;
-	char *path_env, *path_copy, *token;
-	char full_path[1024];
+	char *cmd_path;
 
-	if (strchr(command, '/') != NULL)
+	cmd_path = find_full_path(command);
+
+	if (!cmd_path)
 	{
-		if (access(command, X_OK) != 0)
+		write(STDERR_FILENO, command, strlen(command));
+		write(STDERR_FILENO, ": command not found\n", 21);
+		return (-1);
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		free(cmd_path);
+		return (-1);
+	}
+
+	if (pid == 0)
+	{
+		if (execve(cmd_path, args, env) == -1)
 		{
 			perror(command);
-			return (-1);
-		}
-		pid = fork();
-		if (pid == 0)
-		{
-			execve(command, args, env);
-			perror(command);
+			free(cmd_path);
 			exit(EXIT_FAILURE);
 		}
-		else if (pid > 0)
-			waitpid(pid, &status, 0);
-		else
-			perror("fork");
-		return (0);
 	}
-
-	path_env = getenv("PATH");
-	if (!path_env)
-		return (-1);
-
-	path_copy = strdup(path_env);
-	if (!path_copy)
-		return (-1);
-
-	token = strtok(path_copy, ":");
-	while (token)
+	else
 	{
-		snprintf(full_path, sizeof(full_path), "%s/%s", token, command);
-		if (access(full_path, X_OK) == 0)
-		{
-			pid = fork();
-			if (pid == 0)
-			{
-				execve(full_path, args, env);
-				perror(command);
-				exit(EXIT_FAILURE);
-			}
-			else if (pid > 0)
-			{
-				waitpid(pid, &status, 0);
-			}
-			else
-			{
-				perror("fork");
-			}
-			free(path_copy);
-			return (0);
-		}
-		token = strtok(NULL, ":");
+		waitpid(pid, &status, 0);
 	}
 
-	fprintf(stderr, "%s: command not found\n", command);
-	free(path_copy);
-	return (-1);
+	free(cmd_path);
+	return (0);
 }
