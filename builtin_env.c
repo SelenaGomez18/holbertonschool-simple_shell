7a01c19d80f1a9a59c61a94c3b362c782bce6623
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-08-11 19:31:44 -0500

    Add changes to files exec_cmd and func_environ

--- builtin_env.c
+++ builtin_env.c
@@ -1,14 +1,21 @@
 #include "main.h"
 
+/**
+ * builtin_env - Prints the current environment
+ *
+ * Return: Always 0
+ */
 int builtin_env(void)
 {
-    int i = 0;
+	int i = 0; /* Counter for environment variables */
 
-    while (environ[i] != NULL)
-    {
-        write(STDOUT_FILENO, environ[i], strlen(environ[i]));
-        write(STDOUT_FILENO, "\n", 1);
-        i++;
-    }
-    return (0);
+	/* Loop through each environment variable and print it */
+	while (environ[i] != NULL)
+	{
+		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+
+	return (0);
 }
